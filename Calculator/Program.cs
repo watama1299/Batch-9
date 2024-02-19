35aacc99541f7f81d242291ec69cb83d784b47de
waditya99 <waditya99@gmail.com>
2024-02-19 14:51:49 +0700

    initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program {
+    static void Main(string[] args)
+    {
+        Calculator c = new Calculator();
+        int add = c.Addition(1,2);
+        int sub = c.Subtraction(2,1);
+        int mult = c.Multiplication(2,3);
+        int div = c.Division(4,2);
+
+        Console.WriteLine($"add: {add}, sub: {sub}, mult: {mult}, div: {div}");
+    }
+}
+
+class Calculator {
+    public int Addition(int a, int b) {
+        return a + b;
+    }
+    public int Subtraction(int a, int b) {
+        return a - b;
+    }
+    public int Multiplication(int a, int b) {
+        return a * b;
+    }
+    public int Division(int a, int b) {
+        return a / b;
+    }
+}
---
+++ ProjectA/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program {
+    static void Main() {
+        Cat catA = new Cat();
+        catA.name = "A";
+        catA.colour = "amber";
+        catA.age = 1;
+        catA.isAlive = true;
+
+        Console.WriteLine(catA.name + ", " + catA.colour + ", " + catA.age + ", " + catA.isAlive);
+        catA.Walk();
+
+        Cat catB = new Cat();
+        catB.name = "B";
+        catB.colour = "burgundy";
+        catB.age = 2;
+        catB.isAlive = false;
+
+        Console.WriteLine(catB.name + ", " + catB.colour + ", " + catB.age + ", " + catB.isAlive);
+        catB.Jump();
+    }
+}
+
+class Cat {
+    public string name;
+    public string colour;
+    public int age;
+    public bool isAlive;
+
+
+    public void Walk() {
+        Console.WriteLine(name + " walks");
+    }
+    public void Jump() {
+        Console.WriteLine(name + " jumps");
+    }
+}
