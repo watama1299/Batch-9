1ed924889dae0e103f1861b4629d93f4ab49e517
waditya99 <waditya99@gmail.com>
2024-02-23 14:58:32 +0700

    23/02/24 EOD commit and push

--- Daily/0223/Delegates/Program.cs
+++ Daily/0223/Delegates/Program.cs
@@ -1,4 +1,6 @@
-﻿public delegate void DelegateNoParam();
+﻿using System.Reflection;
+
+public delegate void DelegateNoParam();
 public delegate void DelegateParam(string str);
 public delegate int DelegateReturn();
 
@@ -9,21 +11,38 @@ class Program {
         Lamp lamp = new();
         AirCond airCond = new();
 
+        // Adding parameterless methods to the delegate with no parameters
         DelegateNoParam on = laptop.PushOnButton;
         on += lamp.SwitchOn;
         on += airCond.AcRemoteOn;
 
+        // Adding 1-parameter methods to the delegate with 1 parameter
         DelegateParam off = laptop.PushOffButton;
         off += lamp.SwitchOff;
         off += airCond.AcRemoteOff;
 
+        // Adding methods with int return to the delegate with int return
         DelegateReturn num = laptop.Number;
         num += lamp.Number;
         num += airCond.Number;
 
+        // Calling delegates to run methods
         on();
         off("off");
+        // Will only print return value of the final method in the delegate
         Console.WriteLine(num());
+
+
+        // To get all return values of method, need to get the invocation list
+        // Loop thru invocation list, and run the methods
+        // Store return values somewhere
+        Delegate[] delegates = num.GetInvocationList();
+        int[] delNums = new int[delegates.Length];
+        for(int i = 0; i < delegates.Length; i++) {
+            DelegateReturn temp = (DelegateReturn) delegates[i];
+            delNums[i] = temp();
+            Console.WriteLine($"{temp.Target}.{temp.Method.Name} returns {delNums[i]}");
+        }
     }
 }
 
