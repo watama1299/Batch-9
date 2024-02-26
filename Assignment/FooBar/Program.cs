92d11ea75685feff210e0f7957d1bc272d556121
waditya99 <waditya99@gmail.com>
2024-02-26 11:21:01 +0700

    26/02/24 FooBar Looping program

--- Assignment/FooBar/Program.cs
+++ Assignment/FooBar/Program.cs
@@ -2,12 +2,33 @@
 class Program {
     static void Main(string[] args)
     {
+        bool exit = false;
+        FooBarRunning();
+        while (!exit) {
+            Console.Write("Again? Y/N > ");
+            string e = Console.ReadLine().ToLower();
+            switch (e) {
+                case "n":
+                case "no":
+                    exit = true;
+                    break;
+                case "y":
+                case "yes":
+                    FooBarRunning();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    static void FooBarRunning() {
         // Variable to hold user inputted number
         int num;
         Console.Write("Please input number > ");
 
         // User input validation
-        bool correct = int.TryParse((string) Console.ReadLine(), out num);
+        bool correct = int.TryParse(Console.ReadLine(), out num);
         while (!correct || num < 0) {
             if (!correct) Console.WriteLine("Number not detected.");
             Console.Write("Please input a number greater than or equal to 0 > ");
