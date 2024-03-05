1083c32b0389bb470223103641bf50f5f21779f5
watama1299 <waditya99@gmail.com>
2024-03-05 11:56:43 +0700

    05/03/24 Lunch commit and push

---
+++ Daily/0305/Memory Profiling - String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+class Program {
+    static void Main(string[] args)
+    {
+        string s = "";
+        int loop = 100000;
+
+        Stopwatch sw = new();
+        sw.Start();
+        for (int i = 0; i < loop; i++) {
+            s += "a";
+        }
+        sw.Stop();
+        Console.WriteLine(sw.ElapsedMilliseconds);
+    }
+}
---
+++ Daily/0305/Memory Profiling - StringBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+class Program {
+    static void Main(string[] args)
+    {
+        StringBuilder s = new();
+        int loop = 1_000_000_000;
+
+        Stopwatch sw = new();
+        sw.Start();
+        for (int i = 0; i < loop; i++) {
+            s.Append('a');
+        }
+        sw.Stop();
+        Console.WriteLine(sw.ElapsedMilliseconds);
+    }
+}
