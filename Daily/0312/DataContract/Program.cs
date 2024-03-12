216694873fdd599cfb5ac979973c3df520d8767b
watama1299 <waditya99@gmail.com>
2024-03-12 16:00:23 +0700

    12/03/24 EOD commit and push

---
+++ Daily/0312/DataContract/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+
+[DataContract]
+class Human {
+    [DataMember]
+    private string _name;
+    [DataMember]
+    private int _age;
+    public string Address {get; set;}
+    // try having object
+
+    public Human(string name, int age) {
+        _name = name;
+        _age = age;
+        Address = "default";
+    }
+
+    public string GetName() {
+        return _name;
+    }
+    public int GetAge() {
+        return _age;
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        var human = new Human("Adit", 24);
+        DataContractJsonSerializer serializer = new(typeof(Human));
+        using (FileStream fs = new("file.json", FileMode.Create)) {
+            serializer.WriteObject(fs, human);
+        }
+
+
+        Human result;
+        using (FileStream fs = new("file.json", FileMode.Open)) {
+            result = (Human) serializer.ReadObject(fs);
+        }
+        Console.WriteLine(result.GetName());
+        Console.WriteLine(result.GetAge());
+    }
+}
---
+++ Daily/0312/Serializers/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+
+public class HumanJson {
+    public string Name {get; set;}
+    public int Age {get; set;}
+    public HumanJson(string name, int age) {
+        Name = name;
+        Age = age;
+    }
+}
+
+public class HumanXML {
+    public string Name {get; set;}
+    public int Age;
+    public HumanXML() {}
+    public HumanXML(string name, int age) {
+        Name = name;
+        Age = age;
+    }
+
+}
+
+public class Program {
+    static void Main(string[] args) {
+        var hj1 = new HumanJson("Kinara", 25);
+		var hj2 = new HumanJson("Dion", 22);
+		var hj3 = new HumanJson("Gracia", 22);
+		var humans = new List<HumanJson> { hj1, hj2, hj3 };
+
+		string json = JsonSerializer.Serialize(humans);
+		using (StreamWriter sw = new("file.json")) {
+			sw.Write(json);
+		}
+
+        string resultJSON;
+        using (StreamReader sr = new("file.json")) {
+            resultJSON = sr.ReadToEnd();
+        }
+        HumanJson[] humanJsons = JsonSerializer.Deserialize<HumanJson[]>(resultJSON);
+        foreach (var hj in humanJsons) {
+            Console.WriteLine(hj.Name);
+            Console.WriteLine(hj.Age);
+        }
+
+
+
+        var hx1 = new HumanXML("Adit", 24);
+        var hx2 = new HumanXML("Rafif", 25);
+        var hx3 = new HumanXML("Yudha", 25);
+        var hxs = new List<HumanXML> {hx1, hx2, hx3};
+        var xml = new XmlSerializer(typeof(List<HumanXML>));
+        using (StreamWriter sw = new StreamWriter("file.xml")) {
+            xml.Serialize(sw, hxs);
+        }
+        HumanXML[] humanXMLs;
+        using (StreamReader sr = new("file.xml")) {
+            humanXMLs = (HumanXML[]) xml.Deserialize(sr);
+        }
+        foreach (var hx in humanXMLs) {
+			Console.WriteLine(hx.Name);
+			Console.WriteLine(hx.Age);
+        }
+    }
+}
